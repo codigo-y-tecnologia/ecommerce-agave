f36e60b37b8219faa4489c805793279f87ccb3a9
JoseMendoza2003 <119531621+JoseMendoza2003@users.noreply.github.com>
2025-09-29 15:24:43 -0600

    feat: implementar módulo completo de categorías

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
 
 
 Route::get('/', function () {
@@ -17,3 +18,6 @@ Route::get('/usuarios/crear', [AuthController::class, 'showRegister'])->name('us
 Route::post('/usuarios', [AuthController::class, 'register'])->name('usuarios.store');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::resource('/categorias', CategoriaController::class);
+
