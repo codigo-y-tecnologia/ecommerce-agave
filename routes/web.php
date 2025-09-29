43a3c81326bbd7d5abc961cdad934273b5fc72f5
codigo y tecnologia <codigoytecnologia1@gmail.com>
2025-09-29 14:15:30 -0600

    feat(carrito): agregar vista index del carrito, rutas y controlador inicial del carrito

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\CarritoController;
+use App\Http\Controllers\CheckoutController;
 
 Route::get('/', function () {
     return view('inicio');
@@ -17,3 +18,16 @@ Route::get('/usuarios/crear', [AuthController::class, 'showRegister'])->name('us
 Route::post('/usuarios', [AuthController::class, 'register'])->name('usuarios.store');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// --------------------
+// Rutas de Carrito
+// --------------------
+Route::get('/carrito', [CarritoController::class, 'index'])->name('carrito.index'); // ver carrito
+Route::post('/carrito/{producto}', [CarritoController::class, 'store'])->name('carrito.store'); // agregar producto
+Route::put('/carrito/{detalle}', [CarritoController::class, 'update'])->name('carrito.update'); // actualizar cantidad
+Route::delete('/carrito/{detalle}', [CarritoController::class, 'destroy'])->name('carrito.destroy'); // eliminar producto
+
+// --------------------
+// Rutas de Checkout
+// --------------------
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
