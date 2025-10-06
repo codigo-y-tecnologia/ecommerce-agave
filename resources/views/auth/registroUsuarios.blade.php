73a60cd9fa88a8712befdf6d49a976e33d180099
codigo y tecnologia <codigoytecnologia1@gmail.com>
2025-10-06 17:38:05 -0600

    Refactor: Agregar validaciones a los campos del formulario

--- resources/views/auth/registroUsuarios.blade.php
+++ resources/views/auth/registroUsuarios.blade.php
@@ -30,27 +30,27 @@
 
         <div class="mb-3">
             <label for="vNombre" class="form-label">Nombre *</label>
-            <input type="text" id="vNombre" name="vNombre" class="form-control" value="{{ old('vNombre') }}">
+            <input type="text" id="vNombre" name="vNombre" class="form-control" value="{{ old('vNombre') }}" required>
         </div>
 
         <div class="mb-3">
             <label for="vApaterno" class="form-label">Apellido Paterno *</label>
-            <input type="text" id="vApaterno" name="vApaterno" class="form-control" value="{{ old('vApaterno') }}">
+            <input type="text" id="vApaterno" name="vApaterno" class="form-control" value="{{ old('vApaterno') }}" required>
         </div>
 
         <div class="mb-3">
             <label for="vAmaterno" class="form-label">Apellido Materno *</label>
-            <input type="text" id="vAmaterno" name="vAmaterno" class="form-control" value="{{ old('vAmaterno') }}">
+            <input type="text" id="vAmaterno" name="vAmaterno" class="form-control" value="{{ old('vAmaterno') }}" required>
         </div>
 
         <div class="mb-3">
             <label for="vEmail" class="form-label">Correo Electrónico *</label>
-            <input type="email" id="vEmail" name="vEmail" class="form-control" value="{{ old('vEmail') }}">
+            <input type="email" id="vEmail" name="vEmail" class="form-control" value="{{ old('vEmail') }}" required>
         </div>
 
         <div class="mb-3">
             <label for="vPassword" class="form-label">Contraseña *</label>
-            <input type="password" id="vPassword" name="vPassword" class="form-control" value="{{ old('vPassword') }}">
+            <input type="password" id="vPassword" name="vPassword" class="form-control" value="{{ old('vPassword') }}" required>
             <small id="passwordStrengthText" class="form-text"></small>
     <div id="passwordStrengthBar" class="progress mt-1" style="height: 6px;">
         <div class="progress-bar" role="progressbar"></div>
@@ -59,16 +59,16 @@
 
         <div class="mb-3">
     <label for="vPassword_confirmation" class="form-label">Confirmar Contraseña *</label>
-    <input type="password" id="vPassword_confirmation" name="vPassword_confirmation" class="form-control" value="{{ old('vPassword_confirmation') }}">
+    <input type="password" id="vPassword_confirmation" name="vPassword_confirmation" class="form-control" value="{{ old('vPassword_confirmation') }}" required>
 </div>
 
         <div class="mb-3">
             <label for="dFecha_nacimiento" class="form-label">Fecha de Nacimiento *</label>
-            <input type="date" id="dFecha_nacimiento" name="dFecha_nacimiento" class="form-control" value="{{ old('dFecha_nacimiento') }}" >
+            <input type="date" id="dFecha_nacimiento" name="dFecha_nacimiento" class="form-control" value="{{ old('dFecha_nacimiento') }}" required>
         </div>
 
         <div class="mb-3 form-check">
-            <input type="checkbox" id="terminos" name="terminos" id="terminos" class="form-check-input" value="1" {{ old('terminos') ? 'checked' : '' }}>
+            <input type="checkbox" id="terminos" name="terminos" id="terminos" class="form-check-input" value="1" {{ old('terminos') ? 'checked' : '' }} required>
             <label class="form-check-label" for="terminos">
                 Acepto los términos y condiciones y confirmo que soy mayor de edad.
             </label>
