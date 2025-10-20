a80890856641c5b042b0f7cf3d2be8abe76b2158
codigo y tecnologia <codigoytecnologia1@gmail.com>
2025-10-19 18:41:36 -0600

    d

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'cors',
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
